88381f5d830786144643363693d33a9a7e83e829
PutriHabibahDevina <putrihdm13@gmail.com>
2024-03-30 21:39:07 +0700

    first commit

---
+++ script/main.js
@@ -0,0 +1,11 @@
+//Menggunakan sweetalert2 untuk memunculkan pop up
+function sweetAlert(){
+    sweetAlert.fire({
+        title: "Order Success",
+        text: "Berhasil",
+        icon: "success"
+    });
+}
+
+//Memanggil fungsi dan parameternya
+sweetAlert();
